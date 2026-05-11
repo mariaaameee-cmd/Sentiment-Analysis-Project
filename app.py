5fae42ab62789d7400098a2d8c71a1613411befe
mariaaameee-cmd <mariaaameee@gmail.com>
2026-05-11 17:28:07 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -469,4 +469,7 @@ if __name__ == "__main__":
 
     PORT = int(os.environ.get("PORT", 5000))
 
-    app.run(host="0.0.0.0", port=PORT)
+    if __name__ == "__main__":
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
